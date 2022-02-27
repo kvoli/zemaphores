4f6e634a18b5afee72801e2f42f28813d4483c82
kvoli <austen.mcclernon@gmail.com>
2022-02-27 14:53:55 -0500

    add rendevous

---
+++ chapter3/rendevous.zig
@@ -0,0 +1,50 @@
+const std = @import("std");
+
+// We want to guarantee that a1 > b2 /\ b1 > a2
+// A: a1   B: b1
+//    a2      b2
+// i.e. a Rendevous just following a1, b1.
+// A: a1   B: b1
+// ---[Rendevous]---
+//    a2      b2
+//
+const Thread = std.Thread;
+const Semaphore = @import("semaphore.zig").Semaphore;
+
+var log = std.ArrayList(i32).init(std.testing.allocator);
+
+const Signaller = struct {
+    sem: Semaphore = .{},
+    name: []const u8,
+
+    fn f(self: *Signaller, other: *Semaphore) anyerror!void {
+        std.debug.print("\nstatement {s}1\n", .{self.name});
+        try log.append(1);
+        try other.inc();
+        try self.sem.dec();
+        std.debug.print("\nstatement {s}2\n", .{self.name});
+        try log.append(2);
+    }
+};
+
+var semA = Semaphore{ .tickets = 0 };
+var semB = Semaphore{ .tickets = 0 };
+
+fn a() anyerror!void {
+    var sigA: Signaller = Signaller{ .sem = semA, .name = "a" };
+    try sigA.f(&semB);
+}
+
+fn b() anyerror!void {
+    var sigB: Signaller = Signaller{ .sem = semB, .name = "b" };
+    try sigB.f(&semA);
+}
+
+test "Rendevous" {
+    var threadA: Thread = try Thread.spawn(.{}, a, .{});
+    var threadB: Thread = try Thread.spawn(.{}, b, .{});
+    threadA.join();
+    threadB.join();
+    //try std.testing.expectEqualSlices(i32, [_]i32{ 1, 1, 2, 2 }, log.items);
+    log.deinit();
+}
---
+++ chapter3/semaphore.zig
@@ -0,0 +1,87 @@
+const std = @import("std");
+const Thread = std.Thread;
+const Mutex = Thread.Mutex;
+const Atomic = std.atomic.Atomic;
+
+// Condition mimics pthread_cond_signal within the runtime.
+pub const Condition = struct {
+    pending: bool = false,
+    queue_list: QueueList = .{},
+    queue_mu: Mutex = .{},
+
+    const QueueList = std.SinglyLinkedList(QueueItem);
+
+    pub const QueueItem = struct {
+        futex: i32 = 0,
+
+        fn wait(cond: *@This()) void {
+            while (@atomicLoad(i32, &cond.futex, .Acquire) == 0) {
+                std.atomic.spinLoopHint();
+            }
+        }
+
+        fn notify(cond: *@This()) void {
+            @atomicStore(i32, &cond.futex, 1, .Release);
+        }
+    };
+
+    pub fn wait(cond: *Condition, mu: *Mutex) void {
+        var waiter = QueueList.Node{ .data = .{} };
+
+        cond.queue_mu.lock();
+        defer cond.queue_mu.unlock();
+
+        cond.queue_list.prepend(&waiter);
+        @atomicStore(bool, &cond.pending, true, .SeqCst);
+
+        mu.unlock();
+        waiter.data.wait();
+        mu.lock();
+    }
+
+    pub fn signal(cond: *Condition) void {
+        if (@atomicLoad(bool, &cond.pending, .SeqCst) == false) {
+            return;
+        }
+
+        const maybe_waiter = blk: {
+            cond.queue_mu.lock();
+            defer cond.queue_mu.unlock();
+
+            const maybe_waiter = cond.queue_list.popFirst();
+            @atomicStore(bool, &cond.pending, cond.queue_list.first != null, .SeqCst);
+            break :blk maybe_waiter;
+        };
+
+        if (maybe_waiter) |waiter|
+            waiter.data.notify();
+    }
+};
+
+pub const Semaphore = struct {
+    tickets: i32 = 0,
+    mu: Mutex = .{},
+    cond: Condition = .{},
+
+    pub fn inc(self: *Semaphore) anyerror!void {
+        self.mu.lock();
+        defer self.mu.unlock();
+
+        while (self.tickets == 0) {
+            self.cond.wait(&self.mu);
+        }
+
+        self.tickets -= 1;
+        if (self.tickets > 0) {
+            self.cond.signal();
+        }
+    }
+
+    pub fn dec(self: *Semaphore) anyerror!void {
+        self.mu.lock();
+        defer self.mu.unlock();
+
+        self.tickets += 1;
+        self.cond.signal();
+    }
+};
